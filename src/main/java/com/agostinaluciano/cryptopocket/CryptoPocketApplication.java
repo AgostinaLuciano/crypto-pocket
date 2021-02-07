d4e49adf13b6590d945c533dc396bdab25bd787a
Agostina Luciano <agostinalluciano@gmail.com>
2021-02-07 13:34:25 -0300

    first commit

---
+++ src/main/java/com/agostinaluciano/cryptopocket/CryptoPocketApplication.java
@@ -0,0 +1,13 @@
+package com.agostinaluciano.cryptopocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agostinaluciano/cryptopocket/CryptoPocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agostinaluciano.cryptopocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
