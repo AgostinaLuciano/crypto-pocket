7f39b5f71a97ac80cf149e8326d716e6a75b539f
Agostina Luciano <agostinalluciano@gmail.com>
2021-03-03 20:40:49 -0300

    getUsers

--- src/main/java/com/agostinaluciano/cryptopocket/api/UserController.java
+++ src/main/java/com/agostinaluciano/cryptopocket/api/UserController.java
@@ -2,7 +2,9 @@ package com.agostinaluciano.cryptopocket.api;
 
 import com.agostinaluciano.cryptopocket.domain.User;
 import com.agostinaluciano.cryptopocket.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +14,26 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-  private UserService userService;
+    private UserService userService;
 
-  public UserController(UserService userService) {
-    this.userService = userService;
-  }
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-  @GetMapping
-    public List<User> getAll(){
-    return userService.getAll();
+    @GetMapping
+    public List<User> getAll() {
+        return userService.getAll();
 
-  }
+    }
 
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getOne(@PathVariable("id") int id) {
+
+        User user = userService.getOne(id);
+        if (user != null) {
+            return ResponseEntity.ok(user);
+        }
+        else
+            return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/com/agostinaluciano/cryptopocket/repositories/impl/UserRepositoryImpl.java
+++ src/main/java/com/agostinaluciano/cryptopocket/repositories/impl/UserRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.agostinaluciano.cryptopocket.repositories.impl;
 import com.agostinaluciano.cryptopocket.domain.User;
 import com.agostinaluciano.cryptopocket.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -26,4 +27,18 @@ public class UserRepositoryImpl implements UserRepository {
                         rs.getString("password"), rs.getString("email") ));
         return result;
     }
-}
+
+    @Override
+    public User getOne(int id) {
+        User user = null;
+       try{ user = jdbcTemplate.query("SELECT id, username, password, email FROM \"users\" WHERE id = ?",
+               (rse, rownum) ->  new User(rse.getInt("id"), rse.getString("username"),
+                    rse.getString("password"), rse.getString("email")), id).get(0);
+    }
+       catch (IndexOutOfBoundsException e) {
+           e.printStackTrace();
+       }
+        return user;
+
+
+    }}
