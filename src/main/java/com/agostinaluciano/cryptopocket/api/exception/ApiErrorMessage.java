85e0c441234be2fff4c8bb6982dd6d8552210421
AgostinaLuciano <agostinalluciano@gmail.com>
2021-04-14 12:15:52 -0300

    refactoring message in UserNotFoundException

--- src/main/java/com/agostinaluciano/cryptopocket/api/exception/ApiErrorMessage.java
+++ src/main/java/com/agostinaluciano/cryptopocket/api/exception/ApiErrorMessage.java
@@ -2,10 +2,13 @@ package com.agostinaluciano.cryptopocket.api.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
-@Data//
-@AllArgsConstructor//GENERAN GETTER Y SETTER Y CONSTRUCTOR
+@Data
+@AllArgsConstructor
 public class ApiErrorMessage {
+
     private String errorMessage;
 
 
